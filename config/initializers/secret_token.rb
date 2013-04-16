851bd71ee7ca2ce3e1f50e97b04d413f3ec91636
Saulius Grigaliunas <saulius@ninja.lt>
2013-04-16 15:58:08 +0300

    Initial commit, add Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TradeTariffAdmin::Application.config.secret_token = 'fe3757599549043ce0bad252b0088289054f6e46ed5bfcd1e27df264ed724f39948281969defb39cbd250c600d67dcdbcec9b58d29419969c93d3b1a96b4e2c3'
