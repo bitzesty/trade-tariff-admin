071682348c9fd3fec4e0fecf50408915acf5dcd5
Saulius Grigaliunas <saulius@ninja.lt>
2013-04-18 11:05:24 +0300

    More GDS SSO compatibility

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < Sequel::Model
   include GDS::SSO::User
 
+  def self.find_by_uid(uid)
+    find(uid: uid)
+  end
+
+  def remotely_signed_out?
+    remotely_signed_out
+  end
+
   def update_attribute(attribute, value)
     update({ attribute => value })
   end
