5d56d71d4b482d0c0350f9fc5071b3f2adde592a
Saulius Grigaliunas <saulius@ninja.lt>
2013-07-30 11:43:09 +0300

    Further remove all DB related pieces

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
-RAILS_ENV=test bundle exec rake db:create
-RAILS_ENV=test bundle exec rake db:load
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
 exit $RESULT
