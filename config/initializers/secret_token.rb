5ffbb9ba108ed7d5c747bce85e33e70d11c4f2f3
Saulius Grigaliunas <saulius@ninja.lt>
2013-07-31 16:37:18 +0300

    Add secret_key_base for encrypted cookies

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 TradeTariffAdmin::Application.config.secret_token = 'fe3757599549043ce0bad252b0088289054f6e46ed5bfcd1e27df264ed724f39948281969defb39cbd250c600d67dcdbcec9b58d29419969c93d3b1a96b4e2c3'
+TradeTariffAdmin::Application.config.secret_key_base = 'aa5712faf8f6ef63d90f6581c53072ef2f36714d9ca15fbb55bfae7e42c1183ed4bde59a4663df9bd41dd949aec62b3b0f045de05c01352ebc64fd40a83fc625'
