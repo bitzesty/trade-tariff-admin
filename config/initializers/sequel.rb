3ae8614433d48a42c6ec18c329c176615262ae3d
Saulius Grigaliunas <saulius@ninja.lt>
2013-07-31 15:01:14 +0300

    Add seeds

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,1 +1,3 @@
+require 'sequel_rails/sequel/plugins/rails_extensions'
+
 Sequel::Model.plugin :rails_extensions
---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.new { |u|
+  u.name = 'Winston'
+  u.uid = 1
+  u.email = 'winston@alphagov.co.uk'
+}.save
