f08b2af4d6888bbb660213aba5eb0747127c39c7
Saulius Grigaliunas <saulius@ninja.lt>
2013-08-06 11:31:07 +0300

    Persist users access_token for API requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,21 @@ class User < Sequel::Model
 
   serialize_attributes :yaml, :permissions
 
+  def self.find_for_gds_oauth(auth_hash)
+    user_params = user_params_from_auth_hash(auth_hash)
+
+    # update details of existing user
+    if user = find(uid: auth_hash["uid"])
+      user.update(user_params)
+    else # Create a new user.
+      create(user_params)
+    end
+  end
+
+  def self.user_params_from_auth_hash(auth_hash)
+    GDS::SSO::User.user_params_from_auth_hash(auth_hash.to_hash).merge(access_token: auth_hash.credentials.token)
+  end
+
   def self.find_by_uid(uid)
     find(uid: uid)
   end
