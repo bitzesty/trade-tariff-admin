bbb0a155e3390a49ba1d97dfe1440b9dcc160ae4
Matthew Ford <matt@bitzesty.com>
2013-09-02 10:20:27 +0000

    production configs for her

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,16 @@ TradeTariffAdmin::Application.configure do
   # Host for Trade Tariff API endpoint
   config.api_host = Plek.new.find("tariff-api")
 
+  config.after_initialize do
+    Her::API.setup url: Rails.application.config.api_host do |c|
+      c.use Faraday::Request::BasicAuthentication, ENV['TRADE_TARIFF_USER'], ENV['TRADE_TARIFF_PASSWORD']
+      c.use Faraday::Request::UrlEncoded
+      c.use Her::Middleware::AcceptJSON
+      c.use Her::Middleware::DefaultParseJSON
+      c.use Faraday::Adapter::NetHttp
+    end
+  end
+
   config.eager_load = true
 
   config.lograge.enabled = true
