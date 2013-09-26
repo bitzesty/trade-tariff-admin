552499287adf05b0592e74d14b533cd16be01fdc
Saulius Grigaliunas <saulius@ninja.lt>
2013-09-26 17:29:27 +0300

    Move Her initialization block to application.rb as other environments need it too

--- config/application.rb
+++ config/application.rb
@@ -78,5 +78,16 @@ module TradeTariffAdmin
     config.action_dispatch.rescue_responses.merge!(
       'Sequel::Plugins::RailsExtensions::ModelNotFound' => :not_found
     )
+
+    config.after_initialize do
+      Her::API.setup url: Rails.application.config.api_host do |c|
+        c.use Faraday::Request::BasicAuthentication, ENV['TRADE_TARIFF_USER'], ENV['TRADE_TARIFF_PASSWORD']
+        c.use Faraday::Request::UrlEncoded
+        c.use Her::Middleware::AcceptJSON
+        c.use Her::Middleware::HeaderMetadataParse # lib/her/middleware/header_metadata_parse.rb
+        c.use Her::Middleware::DefaultParseJSON
+        c.use Faraday::Adapter::NetHttp
+      end
+    end
   end
 end
