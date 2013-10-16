55dc028531e69f99598d8db7b8f283a4c95c59e1
Saulius Grigaliunas <saulius@ninja.lt>
2013-10-16 12:58:26 +0300

    Replace nav_link helper with regexp-based one

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
-  def nav_link(text, link)
-    recognized = Rails.application.routes.recognize_path(link)
-    if recognized[:controller] == params[:controller] && recognized[:action] == params[:action]
-      content_tag(:li, :class => "active") do
-        link_to(text, link)
-      end
-    else
-      content_tag(:li) do
-        link_to(text, link)
-      end
+  def nav_link(name, url, activator = '')
+    opts = {}
+
+    if (activator.is_a?(String) && request.path.start_with?(activator)) ||
+      (activator.is_a?(Regexp) && request.path =~ activator)
+      opts.merge!({ class: 'active' })
+    end
+
+    content_tag :li, opts do
+      link_to name, url
     end
   end
 end
