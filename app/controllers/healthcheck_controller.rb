232b0a788f7a533b4541b5d3506e683bf84718b0
Saulius Grigaliunas <saulius@ninja.lt>
2013-10-18 12:01:04 +0300

    Fix Healthcheck action, check for db connectivity

--- app/controllers/healthcheck_controller.rb
+++ app/controllers/healthcheck_controller.rb
@@ -1,11 +1,6 @@
 class HealthcheckController < ActionController::Base
-  # rescue_from ApiEntity::Error do |e|
-  #   render text: '', status: :error
-  # end
-
   def check
-    # Check API connectivity
-    # Section.all
+    User.first # test db connectivity
     render json: { git_sha1: CURRENT_RELEASE_SHA }
   end
 end
