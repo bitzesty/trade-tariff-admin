ca2af165856f47aaca7eefc066a17dbb08304f0d
Saulius Grigaliunas <saulius@ninja.lt>
2013-10-21 14:59:54 +0300

    Rename session for debugging

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TradeTariffAdmin::Application.config.session_store :cookie_store, key: '_trade-tariff-admin_session'
+TradeTariffAdmin::Application.config.session_store :cookie_store, key: '_trade-tariff-admin_app_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
