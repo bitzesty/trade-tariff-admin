34dd1575628ae3e671c9b261b57d939f8fb686f3
Matthew Ford <matt@bitzesty.com>
2013-11-07 11:56:26 +0000

    fix govspeak preview

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     preview: function(content, output) {
       $.ajax({
         type: 'POST',
-        url: "/trade-tariff-admin/govspeak",
+        url: "/govspeak",
         data: { govspeak: content.val() },
         dataType: 'json'
       }).success(function(data){
