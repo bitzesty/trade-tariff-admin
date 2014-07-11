18777182dd86243bbaadd837973b1a0b72c22dc4
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-07-11 14:33:05 +0100

    Change default dev oauth_id
    Prevent it from conflicting with publisher

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,6 +1,6 @@
 GDS::SSO.config do |config|
   config.user_model   = "User"
-  config.oauth_id     = ENV['TARIFF_ADMIN_OAUTH_ID'] || "abcdefghjasndjkasndpublisher"
+  config.oauth_id     = ENV['TARIFF_ADMIN_OAUTH_ID'] || "abcdefghjasndjkasndtariffadmin"
   config.oauth_secret = ENV['TARIFF_ADMIN_OAUTH_SECRET'] || "secret"
   config.oauth_root_url = Plek.current.find("signon")
 end
