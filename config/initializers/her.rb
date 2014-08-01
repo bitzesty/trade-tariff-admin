5e3fd19d4b20f31718a3ba69be703a0d5ac77eaf
Matthew Ford <matt@bitzesty.com>
2014-08-01 12:11:24 +0100

    Pass dummy token by default in development for BearerTokenAuthentication

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -1,7 +1,7 @@
 Her::API.setup url: Rails.application.config.api_host do |c|
   c.use Faraday::Request::UrlEncoded
   c.use Her::Middleware::AcceptJSON
-  c.use Her::Middleware::BearerTokenAuthentication, ENV['BEARER_TOKEN'] # lib/her/middleware/bearer_token_authentication.rb
+  c.use Her::Middleware::BearerTokenAuthentication, ENV['BEARER_TOKEN'] || "tariff-api-test-token"# lib/her/middleware/bearer_token_authentication.rb
   c.use Her::Middleware::HeaderMetadataParse # lib/her/middleware/header_metadata_parse.rb
   c.use Her::Middleware::FirstLevelParseJSON
   c.use Faraday::Adapter::NetHttp
