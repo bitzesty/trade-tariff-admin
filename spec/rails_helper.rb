2821370b9079ece97f4568ef09065c4807d6f8e0
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-07-06 08:01:29 +0100

    Remove GDS API adapters.
    They don't seem to be used for anything.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,6 @@ SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 require File.expand_path("../../config/environment", __FILE__)
 
 require 'rspec/rails'
-require 'gds_api/test_helpers/content_api'
 require 'pundit/rspec'
 require 'webmock/rspec'
 
@@ -27,11 +26,6 @@ RSpec.configure do |config|
   config.include ApiResponsesHelper
   config.include FactoryGirl::Syntax::Methods
   config.include FeaturesHelper, :type => :feature
-  config.include GdsApi::TestHelpers::ContentApi
-
-  config.before(type: :request) do
-    stub_content_api_default_artefact
-  end
 
   config.before(:suite) do
     Sequel::Model.db.tables.delete_if{|t| t == :schema_migrations }
