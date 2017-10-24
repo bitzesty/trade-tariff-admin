092f33b7cc71ed5111141c2b23ceca134212abf0
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-10-24 11:30:11 +0300

    [redis] add `production?` method for TradeTariffAdmin module to check app env

--- lib/trade_tariff_admin.rb
+++ lib/trade_tariff_admin.rb
@@ -2,4 +2,7 @@ require 'her/middleware/bearer_token_authentication'
 require 'her/middleware/header_metadata_parse'
 
 module TradeTariffAdmin
+  def production?
+    ENV["GOVUK_APP_DOMAIN"] == "tariff-admin-production.cloudapps.digital"
+  end
 end
