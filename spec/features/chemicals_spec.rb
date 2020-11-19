de41ce770c5869ee53dd7ea6313ba633d3208720
Christopher Unger <christopher.unger@bitzesty.com>
2020-11-18 20:12:00 -0500

    Add specs for chemical#create and chemical#update

--- spec/features/chemicals_spec.rb
+++ spec/features/chemicals_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-describe "Chemical management" do
+describe 'Chemical management' do
   let!(:user) { create :user, :gds_editor }
-  let(:chemical_name_mock_body) {
+  let(:chemical_name_mock_body) do
     {
       data: [
         {
@@ -15,32 +15,30 @@ describe "Chemical management" do
         }
       ]
     }
-  }
+  end
 
-  before {
-    stub_api_for(Chemical) { |stub|
-      stub.get("/admin/chemicals") { |_env|
+  before do
+    stub_api_for(Chemical) do |stub|
+      stub.get('/admin/chemicals') do |_env|
         jsonapi_success_response('chemical', [])
-      }
-    }
+      end
+    end
     stub_request(:get, "http://api.example.org/admin/chemicals/#{chemical.id}/chemical_names").to_return(status: 200, body: chemical_name_mock_body.to_json, headers: {})
-  }
+  end
 
-  describe "Chemical mapping create" do
+  describe 'Chemical mapping create' do
     let(:chemical) { build :chemical }
     let(:gn1) { build :commodity }
     let(:gn2) { build :commodity }
     specify do
-      stub_api_for(Chemical) { |stub|
-        # POST     /admin/chemicals/:chemical_id/map/:goods_nomenclature_sid
-        stub.post("/admin/chemicals/#{chemical.id}/map/#{gn1.id}") { |_env|
+      stub_api_for(Chemical) do |stub|
+        stub.post("/admin/chemicals/#{chemical.id}/map/#{gn1.id}") do |_env|
           jsonapi_success_response('chemical', [chemical_attributes(gn1)])
-        }
-        # GET     /admin/chemicals/:id
-        stub.get("/admin/chemicals/#{chemical.id}") { |_env|
+        end
+        stub.get("/admin/chemicals/#{chemical.id}") do |_env|
           jsonapi_success_response('chemical', chemical_attributes(gn1))
-        }
-      }
+        end
+      end
       refute map_created_for(chemical)
 
       create_map_for chemical, goods_nomenclature_item_id: gn1.id
@@ -50,33 +48,25 @@ describe "Chemical management" do
     end
   end
 
-  describe "Chemical mapping update" do
+  describe 'Chemical mapping update' do
     let(:gn1) { build :commodity }
     let(:gn2) { build :commodity }
     let(:chemical) { build :chemical }
     specify do
-      stub_api_for(Chemical) { |stub|
-        # GET     /admin/chemicals/:id
-        stub.get("/admin/chemicals/#{chemical.id}") { |_env|
+      stub_api_for(Chemical) do |stub|
+        stub.get("/admin/chemicals/#{chemical.id}") do |_env|
           jsonapi_success_response('chemical', chemical_attributes(gn1))
-        }
-      }
+        end
+      end
       edit_map_for(chemical, gn1.goods_nomenclature_item_id, gn2.goods_nomenclature_item_id) do
-        stub_api_for(Chemical) { |stub|
-          # PATCH     /admin/chemicals/:chemical_id/map/:goods_nomenclature_item_id
-          # PUT       /admin/chemicals/:chemical_id/map/:goods_nomenclature_item_id
-          stub.patch("/admin/chemicals/#{chemical.id}/map/#{gn1.id}?goods_nomenclature_item_id=#{gn2.goods_nomenclature_item_id}") { |_env|
+        stub_api_for(Chemical) do |stub|
+          stub.patch("/admin/chemicals/#{chemical.id}/map/#{gn1.id}?goods_nomenclature_item_id=#{gn2.goods_nomenclature_item_id}") do |_env|
             jsonapi_success_response('chemical', chemical_attributes(gn2))
-          }
-          # GET     /admin/chemicals/:id
-          stub.get("/admin/chemicals/#{chemical.id}") { |_env|
+          end
+          stub.get("/admin/chemicals/#{chemical.id}") do |_env|
             jsonapi_success_response('chemical', chemical_attributes(gn2))
-          }
-          # DELETE /admin/chemicals/:id/map/:goods_nomenclature_item_id
-          # stub.delete("/admin/chemicals/#{chemical.id}/map/#{gn1.id}") { |_env|
-          #   jsonapi_success_response('chemical', chemical_attributes(gn1))
-          # }
-        }
+          end
+        end
       end
 
       expect(page).to have_content "Mapping for #{gn2.goods_nomenclature_item_id} was updated"
@@ -85,34 +75,87 @@ describe "Chemical management" do
     end
   end
 
-  describe "Chemical mapping delete" do
+  describe 'Chemical mapping delete' do
     let(:gn1) { build :commodity }
     let(:chemical) { build :chemical }
     specify do
-
-      stub_api_for(Chemical) { |stub|
-        # GET     /admin/chemicals/:id
-        stub.get("/admin/chemicals/#{chemical.id}") { |_env|
+      stub_api_for(Chemical) do |stub|
+        stub.get("/admin/chemicals/#{chemical.id}") do |_env|
           jsonapi_success_response('chemical', chemical_attributes(gn1))
-        }
-      }
+        end
+      end
 
-      delete_map_for(chemical, gn1.goods_nomenclature_item_id) do
-        stub_api_for(Chemical) { |stub|
-          # DELETE     /admin/chemicals/:chemical_id/map/:goods_nomenclature_sid
-          stub.delete("/admin/chemicals/#{chemical.id}/map/#{gn1.id}") { |_env|
+      delete_map_for(chemical) do
+        stub_api_for(Chemical) do |stub|
+          stub.delete("/admin/chemicals/#{chemical.id}/map/#{gn1.id}") do |_env|
             jsonapi_success_response('chemical', chemical_attributes(nil))
-          }
-          # GET     /admin/chemicals/:id
-          stub.get("/admin/chemicals/#{chemical.id}") { |_env|
+          end
+          stub.get("/admin/chemicals/#{chemical.id}") do |_env|
             jsonapi_success_response('chemical', chemical_attributes(nil))
-          }
-        }
-
+          end
+        end
       end
 
       expect(page).to have_content "Mapping for #{gn1.goods_nomenclature_item_id} was deleted"
       expect(page).to have_no_content "Commodity #{gn1.goods_nomenclature_item_id}"
+    end
+  end
+
+  describe 'Chemical create' do
+    let(:gn1) { build :commodity }
+    let(:chemical) { build :chemical }
+    specify do
+      stub_api_for(Chemical) do |stub|
+        stub.get('/admin/chemicals') do |_env|
+          jsonapi_success_response('chemicals', [chemical_attributes(gn1)])
+        end
+      end
+      new_cas = '7-7-7'
+      new_name = 'Unotainium'
+      create_for(chemical, new_cas, new_name) do
+        stub_api_for(Chemical) do |stub|
+          stub.post('/admin/chemicals', { data: { type: 'chemicals', attributes: { cas: new_cas, name: new_name } } }) do |_env|
+            jsonapi_success_response('chemical', chemical_attributes(nil))
+          end
+          stub.get("/admin/chemicals/#{chemical.id}") do |_env|
+            jsonapi_success_response('chemical', chemical_attributes(nil))
+          end
+        end
+      end
+
+      expect(page).to have_content 'Chemical was created'
+    end
+  end
+
+  describe 'Chemical update' do
+    let(:gn1) { build :commodity }
+    let(:chemical) { build :chemical }
+    specify do
+      stub_api_for(Chemical) do |stub|
+        stub.get('/admin/chemicals') do |_env|
+          jsonapi_success_response('chemicals', [chemical_attributes(gn1)])
+        end
+        stub.get("/admin/chemicals/#{chemical.id}") do |_env|
+          jsonapi_success_response('chemical', chemical_attributes(gn1))
+        end
+      end
+      new_cas = '6-6-6'
+      new_name = 'Kyber (crystal)'
+      update_for(chemical, new_cas, new_name) do
+        stub_api_for(Chemical) do |stub|
+          stub.patch("/admin/chemicals/#{chemical.id}?cas=#{new_cas}") do |_env|
+            jsonapi_success_response('chemical', chemical_attributes(gn1))
+          end
+          stub.patch("/admin/chemicals/#{chemical.id}?chemical_name_id=#{chemical.chemical_names.first.id}&new_chemical_name=#{new_name}") do |_env|
+            jsonapi_success_response('chemical', chemical_attributes(gn1))
+          end
+          stub.get("/admin/chemicals/#{chemical.id}") do |_env|
+            jsonapi_success_response('chemical', chemical_attributes(gn1))
+          end
+        end
+      end
+
+      expect(page).to have_content "Chemical #{chemical.cas} was updated"
     end
   end
 
@@ -139,7 +182,7 @@ describe "Chemical management" do
     click_button 'Update mapping'
   end
 
-  def delete_map_for(chemical, old_gn)
+  def delete_map_for(chemical)
     ensure_on chemical_edit_commodity_mapping_path(chemical)
 
     yield if block_given?
@@ -155,11 +198,33 @@ describe "Chemical management" do
   def update_map_for(chemical, old_gn, new_gn)
     ensure_on chemical_edit_commodity_mapping_path(chemical)
 
+    yield if block_given?
+
     fill_in "Commodity #{old_gn}", with: new_gn
+    click_button 'Update mapping'
+  end
+
+  def create_for(_chemical, new_cas, new_name)
+    ensure_on chemicals_path
+
+    fill_in 'CAS number', with: new_cas
+    fill_in 'Name', with: new_name
 
     yield if block_given?
 
-    click_button 'Update mapping'
+    click_button 'Create new chemical'
+  end
+
+  def update_for(chemical, new_cas, new_name)
+    ensure_on chemical_edit_commodity_mapping_path(chemical)
+
+    yield if block_given?
+
+    fill_in 'CAS Number', with: new_cas
+    click_button 'Update chemical'
+
+    fill_in 'Name', with: new_name
+    click_button 'Update name'
   end
 
   def map_created_for(chemical)
@@ -167,20 +232,20 @@ describe "Chemical management" do
     page.has_selector?("chemical_Update_commodity_mapping_#{gn1.goods_nomenclature_item_id}")
   end
 
-  def chemical_attributes(gn)
+  def chemical_attributes(commodity)
     {
-      "id": "#{chemical.id}",
-      "cas": "#{chemical.cas}",
-      "name": "#{chemical.name}",
-      "goods_nomenclature_map": goods_nomenclature_map(gn)
+      "id": chemical.id.to_s,
+      "cas": chemical.cas.to_s,
+      "name": chemical.name.to_s,
+      "goods_nomenclature_map": goods_nomenclature_map(commodity)
     }
   end
 
-  def goods_nomenclature_map(gn)
-    return nil unless gn
+  def goods_nomenclature_map(commodity)
+    return nil unless commodity
 
     {
-      "#{gn.id}": "#{gn.goods_nomenclature_item_id}"
+      "#{commodity.id}": commodity.goods_nomenclature_item_id.to_s
     }
   end
 end
